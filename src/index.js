17bcbf42e5d5e4dacf59bcc9ee3a672ee2a7c2da
Dashnyam <lord_dashka277@yahoo.com>
2020-12-07 19:15:37 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./pages/App";
+import reportWebVitals from "./reportWebVitals";
+
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+
+import burgerReducer from "./redux/reducer/burgerReducer";
+import orderReducer from "./redux/reducer/orderReducer";
+import signupLoginReducer from "./redux/reducer/signupLoginReducer";
+
+const loggerMiddleware = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log("MyLoggerMiddleware : Dispatching : ", action);
+      console.log("MyLoggerMiddleware : State Before ", store.getState());
+      const result = next(action);
+      console.log("MyLoggerMiddleware : State After ", store.getState());
+      return result;
+    };
+  };
+};
+
+const reducers = combineReducers({
+  burgerReducer,
+  orderReducer,
+  signupLoginReducer,
+});
+
+const middlewares = [loggerMiddleware, thunk];
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(...middlewares))
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
